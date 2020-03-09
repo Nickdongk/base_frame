785bc979ef7cc639a4f372965625b851f28826f8
nick_dong <dongdongqiangqiangkun@gmail.com>
2020-03-09 15:45:49 +0800

    feat: add notes to header files
    Signed-off-by: nick_dong <dongdongqiangqiangkun@gmail.com>

--- map/map.h
+++ map/map.h
@@ -1,16 +1,5 @@
 /***************************************************************************
  *
- * Copyright (c) 2016 zkdnfcf, Inc. All Rights Reserved
- * $Id$
- *
- **************************************************************************/
-
- /**
- * @file hash.h
- * @author zk(zkdnfc@163.com)
- * @date 2016/05/31 18:26:01
- * @version $Revision$
- * @brief
  *
  **/
 #ifndef _MAP_H
@@ -19,12 +8,17 @@
 #include <rbtree.h>
 #include <stddef.h>
 
-
+/**
+ * map head struct
+ */
 struct map {
     struct rb_root root;
     size_t count;
 };
 
+/**
+ * map node struct
+ */
 struct map_n {
     struct rb_node node;
     char *key;
@@ -35,14 +29,73 @@ struct map_n {
 #define map_for_each_node(node, src_map) \
     for(node = map_first(src_map); node != NULL; node = map_next(node))
 
+/**
+ *map_create
+ *@return The pointer to a map
+ *@brief Create a key-value pair map
+ * */
 extern struct map *map_create(void);
+
+/**
+ *map_release
+ *@param map The map is to be released
+ *@brief Release a key-value pair map created by map_create
+ */
 extern void map_release(struct map *map);
 
+/**
+ *map_first
+ *@param map The map that will be checked
+ *@return map node The first map node
+ *@brief Get the first map node from a map.
+ * map node is ordered according its key
+ */
 extern struct map_n *map_first(struct map *map);
+
+/**
+ *map_next
+ *@param map_node The node that will be checked
+ *@return map node The successed map node
+ *@brief Get the node behind the giving node
+ */
 extern struct map_n *map_next(struct map_n *map_node);
+
+/**
+ *map_add
+ *@param map The map that will be checked
+ *@param key The key input
+ *@param val The pointer of value
+ *@param val_len The len of value
+ *@return 0 The key exist, 1 add success, -1 fail
+ *@brief add key-value pair to the map
+ */
 extern int map_add(struct map *map, char* key, void* val, size_t val_len);
+
+/**
+ *map_remove
+ *@param map
+ *@param key The key of node that will be removed
+ *@return 0 The key-value pair is not in the map, 1 remove successfully
+ *@brief remove one key-value pair from the map
+ */
 extern int map_remove(struct map *map, char *key);
-extern void *map_get(struct map *map, char *str, size_t *val_len);
+
+/**
+ *map_get
+ *@param map
+ *@param key
+ *@param val_len The pointer to a value length, used to return value len
+ *@return The pointer to value, the len is in *val_len
+ *@brief Get the value accroding to the giving key
+ */
+extern void *map_get(struct map *map, char *key, size_t *val_len);
+
+/**
+ *map_length
+ *@param map
+ *@return The number of key-value pair in the giving map
+ *@brief Get the number of nodes in the map
+ */
 extern size_t map_length(struct map *map);
 
 #endif  //_MAP_H
--- multimap/multimap.h
+++ multimap/multimap.h
@@ -1,14 +1,20 @@
-#ifndef _MAP_H
-#define _MAP_H
+#ifndef _MULTIMAP_H
+#define _MULTIMAP_H
 
 #include <rbtree.h>
 #include <list.h>
 
+/**
+ * multimap head struct
+ */
 struct multimap {
     struct rb_root root;
     size_t count;
 };
 
+/**
+ * multimap node struct
+ */
 struct multimap_n {
     struct rb_node node;
     char *key;
@@ -17,6 +23,9 @@ struct multimap_n {
     struct multimap *map_in;
 };
 
+/**
+ * multimap value set head struct
+ */
 struct multimap_list_n {
     struct list_head head;
     struct multimap_n *map_n_in;
@@ -26,13 +35,25 @@ struct multimap_list_n {
 
 #define multimap_for_each_keynode(ptr, key_in)  list_for_each_entry(ptr, &key_in->entry, head)
 
+/**
+ *multimap_create
+ *@return The pointer to a multimap
+ *@brief Create a key-multivalue pair map
+ * */
 extern struct multimap *multimap_create(void);
+
+/**
+ *multimap_release
+ *@param src_map The multimap is to be released
+ *@brief Release a key-multivalue pair multimap created by multimap_create
+ */
 extern void multimap_release(struct multimap *src_map);
 
 extern struct multimap_n *map_first(struct multimap *map);
 extern struct multimap_n *map_next(struct multimap_n *map_node);
 extern int multimap_add(struct multimap *map, char* key, void* val, size_t val_len);
 extern struct multimap_n *multimap_get(struct multimap *map, char *key);
+extern int  multimap_remove(struct multimap *map, char *key, void *val, size_t val_len)
 
 #endif  //_MAP_H
 
